63bee9d7c5c1338788c3a3b7181bf791270b8010
sebastianano <105164421+sebastianano@users.noreply.github.com>
2024-01-12 20:01:35 +0100

    simplify camera styles and rename reactivated to backdrop

--- frontend/src/features/camera/Camera.tsx
+++ frontend/src/features/camera/Camera.tsx
@@ -190,7 +190,7 @@ const Camera = () => {
                         height={height}
                 />
                 <canvas className={styles.canvas} ref={canvasRef}/>
-                {!hasWebcamBeenReactivated && <div className={styles.reactivated}></div>}
+                {!hasWebcamBeenReactivated && <div className={styles.backdrop}></div>}
             </div>
     );
 };

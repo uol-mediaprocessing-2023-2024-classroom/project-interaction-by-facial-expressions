607ae75956ebbfd2b1823864c05088bf163a2728
sebastianano <105164421+sebastianano@users.noreply.github.com>
2024-01-24 21:52:11 +0100

    Try using threads to make the emotion detection process non-blocking and possibly speed up the entire image analysis process.

--- backend/src/image_analysis/utils/Debouncer.py
+++ backend/src/image_analysis/utils/Debouncer.py
@@ -1,8 +1,12 @@
+import threading
+
+
 class Debouncer:
-    def __init__(self, limit):
+    def __init__(self, limit, start_in_a_thread=False):
         self.limit = limit
         self.counter = 0
         self.last_value = None
+        self.start_in_a_thread = start_in_a_thread
 
     def __call__(self, fn, value='', *args, **kwargs):
         if value == self.last_value:
@@ -13,4 +17,8 @@ class Debouncer:
 
         if self.counter >= self.limit:
             self.counter = 0
-            return fn(*args, **kwargs)
+
+            if self.start_in_a_thread:
+                threading.Thread(target=fn, args=args, kwargs=kwargs).start()
+            else:
+                fn(*args, **kwargs)
